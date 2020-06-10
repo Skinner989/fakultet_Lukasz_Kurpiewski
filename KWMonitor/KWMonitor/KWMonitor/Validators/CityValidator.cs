0edb5d0e96d4726bbd76de977b01e3fab4f7e1cf
Łukasz Kurpiewski <skinner989@wp.pl>
2020-06-10 23:28:28 +0200

    back-end

---
+++ KWMonitor/KWMonitor/KWMonitor/Validators/CityValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using KoronaWirusMonitor3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KWMonitor.Validators
+{
+    public class CityValidator : AbstractValidator<City>
+    {
+        public CityValidator()
+        {
+            RuleFor(city => (city.Name))
+            .NotEmpty().WithMessage("Nazwa miasta nie może być pusta")
+            .MinimumLength(3).WithMessage("Nazwa miasta nie może być krótsza niż 3 znaki")
+            .MaximumLength(30).WithMessage("Nazwa miasta nie może być dłuższa niż 30 znaków");
+            RuleFor(district => (district.Name))
+            .NotEmpty();
+        }
+    }
+}
---
+++ KWMonitor/KWMonitor/KWMonitor/Validators/DistrictValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using KoronaWirusMonitor3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KWMonitor.Validators
+{
+    public class DistrictValidator : AbstractValidator<District>
+    {
+        public DistrictValidator()
+        {
+            RuleFor(district => (district.Name))
+            .NotEmpty().WithMessage("Nazwa nie może być pusta")
+            .MinimumLength(3).WithMessage("Nazwa nie może być krótsza niż 3 znaki")
+            .MaximumLength(30).WithMessage("Nazwa nie może być dłuższa niż 30 znaków");
+        }
+    }
+}
